6accb4f8b770d85550e3ba1bf88419015dc17dfc
𝐍𝐢𝐯𝐫𝐢𝐭𝐢 𝐌𝐮𝐭𝐡𝐮𝐯𝐚𝐢𝐫𝐚𝐯𝐚𝐧 <nivriti3266@gmail.com>
2025-09-15 20:59:35 +0400

    mobide: save & deploy app/admin/admissions/page.tsx

--- app/admin/admissions/page.tsx
+++ app/admin/admissions/page.tsx
@@ -1,1 +1,5 @@
-export default function Page() { return <div>app/admin/admissions page</div>; }
+function compareParams(param1: number, param2: number): number {
+    return param1 > param2 ? param1 : param2;
+    }
+    }
+}
